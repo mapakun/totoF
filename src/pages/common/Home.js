3db832e43551dc17a221016b2505d02c4c4a0215
mapakun <hayoko9@gmail.com>
2023-05-05 03:06:54 +0900

    remove dummy data

--- src/pages/common/Home.js
+++
@@ -1,9 +0,0 @@
-const Login = () => {
-    return (
-      <div>
-        <p>홈페이지 입니다</p>
-      </div>
-    );
-  };
-  
-  export default Login;
--- src/pages/common/NotFound.js
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-    return (
-      <div>
-        <p>허용하지 않는 접근입니다.</p>
-      </div>
-    );
-  };
-  
-  export default NotFound;
