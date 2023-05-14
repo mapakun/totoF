18d4d213a1b8f02cbb7f91dc8bb58284df85ab45
mapakun <hayoko9@gmail.com>
2023-05-14 19:18:56 +0900

    공통함수 주석 추가

--- src/api/axios/axiosFunc.js
+++ src/api/axios/axiosFunc.js
@@ -1,6 +1,20 @@
 import axios, { AxiosHeaders } from 'axios';
 
-//엑시오스 호출 공통함수
+/*
+@ 엑시오스 공통함수
+@ param
+(1)type : get or post , (2)url : callURL , (3)reqData : body or param , (4)callBack : callBackFuction
+
+ex)
+const test = {inputId:'크앙',inputPw:'그앙'};
+function testCallBack(res)
+      {
+        console.log("콜백함수 테스트 입니다 잘오면 반응합니다.");
+        console.log(res);
+      }
+      
+common.commonAxios("post","/gameinfo/test",test,testCallBack);
+*/
 export function commonAxios(type,url,reqData,callBack)
 {
     const reqType = type
